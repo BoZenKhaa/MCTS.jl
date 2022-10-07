1dec0f752cbc40758c59f272be97a54b4dcde53d
BoZenKhaa <mrkosjan@gmail.com>
2022-10-07 15:06:01 +0200

    Added large gridworld domain to the benchmarks

--- benchmark/vanilla_gridworld.jl
+++ benchmark/vanilla_gridworld.jl
@@ -1,3 +1,11 @@
+function benchmark_action(n, rng, mdp, planner)
+    for i in 1:n
+        state = rand(rng, initialstate(mdp))
+        a = action(planner, state)
+    end
+end
+
+
 n_iter = 1000
 depth = 10
 ec = 10.0
@@ -11,19 +19,16 @@ solver = MCTSSolver(
     sizehint=100_000,
     rng=RNG
 )
-mdp = SimpleGridWorld(;size=(10,10))
-planner = solve(solver, mdp)
 
-function benchmark_action(n, rng, mdp, planner)
-    for i in 1:n
-        state = rand(rng, initialstate(mdp))
-        a = action(planner, state)
-    end
-end
+small_mdp = SimpleGridWorld(;size=(10,10))
+small_planner = solve(solver, small_mdp)
 
 GC.enable_logging(false)
 
 # @benchmark benchmark_action(42, $RNG, $mdp, $planner)
-SUITE["vanilla"]["action"] = @benchmarkable  benchmark_action(42, $RNG, $mdp, $planner)
+SUITE["vanilla"]["action_small"] = @benchmarkable  benchmark_action(42, $RNG, $small_mdp, $small_planner)
 # SUITE["vanilla"]["action"] = @benchmarkable POMDPs.action($planner, $state)
 
+large_mdp = SimpleGridWorld(;size=(100,1000))
+large_planner = solve(solver, large_mdp)
+SUITE["vanilla"]["action_large"] = @benchmarkable  benchmark_action(42, $RNG, $large_mdp, $large_planner)
